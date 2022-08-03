01bbf6ac3bc290d01c899d2a62f208e9030e23ae
kate Gonzalez <magp2606@gmail.com>
2022-08-02 22:18:02 -0500

    Cart Issue Solved!

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,7 +1,6 @@
 import Stripe from 'stripe'
-import { NEXT_PUBLIC_STRIPE_SECRET_KEY } from 'no-commit'
 
-const key = process.env.NODE_ENV === 'development' ? NEXT_PUBLIC_STRIPE_SECRET_KEY : process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY
+const key = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY
 
 const stripe = new Stripe(key)
 
